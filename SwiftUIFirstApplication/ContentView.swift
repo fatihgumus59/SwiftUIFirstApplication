5b97a93c37208febecda0a3dd6fdd3606bc48be8
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-13 06:59:11 +0300

    tex edit

--- SwiftUIFirstApplication/ContentView.swift
+++ SwiftUIFirstApplication/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+            .frame(width: 300,height: 100,alignment: .center)
+            .minimumScaleFactor(0.8)
+            .font(.headline)
+            .foregroundStyle(.orange)
+            .underline(true, color: .black)
+            .kerning(2)
+           
+        
     }
 }
 
