f94528fbda0ce8780eeb38d995b45970f66a96f3
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-13 06:44:37 +0300

    Initial Commit

---
+++ SwiftUIFirstApplication/SwiftUIFirstApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstApplicationApp.swift
+//  SwiftUIFirstApplication
+//
+//  Created by Fatih Gümüş on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
